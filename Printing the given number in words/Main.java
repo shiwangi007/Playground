2cc447ee3dcd638cb2861a160a0dbde5ab5e5435
shiwangi007 <47782196+shiwangi007@users.noreply.github.com>
2019-07-06 12:11:51 +0530

    Test code pushed to repository.

---
+++ Printing the given number in words/Main.java
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+ int num,option;
+  scanf("%d",&option);
+  switch(option)
+  {
+    case 1:
+        printf("One");
+      break;
+    case 2:
+      printf("Two");
+      break;
+    case 3:
+      printf("Three");
+      break;
+    case 4:
+      printf("Four");
+        break;
+        case 5:
+          printf("Five");
+      break;
+    default:
+      printf("Invalid");
+      break;
+      return 0;
+}
+}
---
+++ Sum of 'n' natural numbers using function/Main.java
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+  int n;
+   scanf("%d", &n);
+   printf("%d", sum_of_natural_numbers(n)); // Function call
+  	return 0;
+}
+int sum_of_natural_numbers(int num){// Function definition
+    int sum = 0;
+    for(int i = 1; i <= num; i++){
+        sum = sum + i;
+    }
+    return sum;
+
+  	return 0;
+}
